b266166076c731c8fc3bbdb3897397d30fd07ddf
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-06-24 13:15:35 +0700

    feat: 14

---
+++ src/4.js
@@ -0,0 +1,43 @@
+/**
+ * 14. Longest Common Prefix
+ *
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+	const strsLength = strs.length;
+
+	if (strsLength === 1) {
+		return strs[0];
+	}
+
+	let commonPrefix = "";
+	let stop = false;
+
+	for (let i = 0; ; i++) {
+		const prefix = strs[0]?.charAt(i);
+
+		if (!prefix) {
+			break;
+		}
+
+		for (let j = 1; j < strsLength; j++) {
+			if (strs[j]?.charAt(i) !== prefix) {
+				stop = true;
+				break;
+			}
+		}
+
+		if (stop) {
+			break;
+		}
+
+		commonPrefix += prefix;
+	}
+
+	return commonPrefix;
+};
