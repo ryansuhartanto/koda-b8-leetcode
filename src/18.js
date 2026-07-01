22a50804b8c7492cd7fa95be51355f6b51fb7fd9
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-01 10:53:44 +0700

    feat: 18

---
+++ src/18.js
@@ -0,0 +1,42 @@
+/**
+ * 15. 3Sum
+ *
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ *
+ * Notice that the solution set must not contain duplicate triplets.
+ *
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+	const typed = new Int32Array(nums).sort((a, b) => a - b);
+	const result = [];
+
+	for (let x = 0; x < typed.length - 2; x++) {
+		if (x > 0 && typed[x] === typed[x - 1]) continue; // duplicate
+
+		let left = x + 1;
+		let right = typed.length - 1;
+		const target = -typed[x];
+
+		while (left < right) {
+			const sum = typed[left] + typed[right];
+
+			if (sum === target) {
+				result.push([typed[x], typed[left], typed[right]]);
+				do {
+					left++;
+				} while (left < right && typed[left] === typed[left - 1]);
+				do {
+					right--;
+				} while (left < right && typed[right] === typed[right + 1]);
+			} else if (sum < target) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+	}
+
+	return result;
+};
