e758ef081fd19eb206b53490ebcee9a9412b1559
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-12 17:39:44 +0700

    build: use typescript

---
+++ globals.d.ts
@@ -0,0 +1,6 @@
+/** Definition for singly-linked list, provided by the LeetCode runtime. */
+declare class ListNode {
+	val: number;
+	next: ListNode | null;
+	constructor(val?: number, next?: ListNode | null);
+}
